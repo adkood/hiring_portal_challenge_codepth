482546b99869b0235ec8739a27331fb53cd0e66b
adkood <ashutoshnegi195@gmail.com>
2023-12-23 20:09:02 +0530

    final

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import SingleJob from './components/SingleJob';
 import getAllJobs from './apis/getAllJobs';
 import JobEntry from './components/JobEntry';
-import SearchBar from './components/SearchBar';
 import Signup from './components/SignUp';
 import { useDispatch, useSelector } from 'react-redux';
 import { modalActions } from './store';
@@ -11,16 +10,30 @@ import useAuthState from './apis/useAuthState';
 import useLogout from './apis/useLogout';
 
 function App() {
+  const [searchData, setSearchData] = useState("");
   const [appRender, setAppRender] = useState(0);
   const render = useSelector((state) => state.modal.render);
   const { log } = useAuthState();
   const { logout } = useLogout();
   const dispatch = useDispatch();
-
   const [allJobs, setAllJobs] = useState([]);
+  
   useEffect(() => {
     getAllJobs(setAllJobs);
-  }, []);
+  }, [render]);
+
+  const filterJobs = () => {
+    const filteredJobs = allJobs.filter((job) =>
+        job.title.toLowerCase().includes(searchData.toLowerCase())
+    );
+    setAllJobs(filteredJobs);
+    console.log(filteredJobs);
+  };
+
+  const onChangeHandler = (e) => {
+    setSearchData(e.target.value);
+    filterJobs();
+  };
 
   const ontoggle = () => {
     dispatch(modalActions.signupToggle());
@@ -32,9 +45,6 @@ function App() {
     setAppRender(appRender + 1);
   }
 
-  useEffect(() => {
-  }, [render, appRender]);
-
   return (
     <div className="App">
       <Signup />
@@ -42,7 +52,10 @@ function App() {
         <section className="cont__left">
           <section className='cont__left__search'>
             <h1 style={{ color: "lightseagreen", fontSize: "2rem", margin: "10px" }}>Jobs</h1>
-            <SearchBar width={"80"} height={"40"} color={'lightseagreen'} allJobs={allJobs} setAllJobs={setAllJobs} setAppRender={setAppRender}/>
+            {/* search bar */}
+            <div style={{ borderRadius: "5px", border: `1px solid lightseagreen`, width: `80%`, height: `40%`, color: `lightseagreen` }}>
+              <input onChange={onChangeHandler} placeholder='   search your role...' style={{ borderRadius: "5px", width: "100%", height: "100%", color: `lightseagreen` }} />
+            </div>
             {!log && <button className='btn' onClick={ontoggle}>signup/login</button>}
             {log && <button className='btn' onClick={onLogoutHandler}>logout</button>}
           </section>
--- src/components/SingleJob.js
+++ src/components/SingleJob.js
@@ -1,15 +1,12 @@
-import react from 'react';
-
+import React from 'react';
 import { useToast } from '@chakra-ui/react';
 
-const SingleJob = ({ job, log}) => {
+const SingleJob = ({ job, log }) => {
     const toast = useToast();
 
     const onApplyHandler = () => {
         if (log) {
-
-        }
-        else {
+        } else {
             toast({
                 title: 'You are not Logged In.',
                 status: 'error',
@@ -17,24 +14,37 @@ const SingleJob = ({ job, log}) => {
                 isClosable: true,
             });
         }
-    }
+    };
 
     return (
         <div className='cont__left__singleJob'>
-            <section style={{ width: "40%", height: "100%", display: "flex", margin: "10px", justifyContent: "center", flexDirection: "column" }}>
-                <span>{job.title}</span>
-                <span>{job.description}</span>
-                <span>{job.requiredSkills}</span>
+            <section style={{ width: '40%', height: '100%', display: 'flex', margin: '10px', justifyContent: 'center', flexDirection: 'column' }}>
+                <span>* {job.title}</span>
+                <span>* {job.description}</span>
+                <span>* {job.requiredSkills}</span>
             </section>
-            <section style={{ width: "10%", height: "100%", display: "flex", margin: "10px", justifyContent: "center", flexDirection: "column" }}>
+            <section style={{ width: '10%', height: '100%', display: 'flex', margin: '10px', justifyContent: 'center', flexDirection: 'column' }}>
                 <span>{job.experience}</span>
             </section>
-            <section style={{ width: "15%", height: "100%", display: "flex", margin: "10px", justifyContent: "center", flexDirection: "column" }}>
+            <section style={{ width: '15%', height: '100%', display: 'flex', margin: '10px', justifyContent: 'center', flexDirection: 'column' }}>
                 <span>{job.company}</span>
             </section>
-            <button className='btn__apply' onClick={onApplyHandler}>Apply now</button>
+            {log &&
+                <a href={`${job.link}`}>
+                    <button className='btn__apply'>
+                        Apply
+                    </button>
+                </a>
+            }
+            {
+                !log &&
+                <button className='btn__apply' onClick={onApplyHandler}>
+                    Apply
+                </button>
+
+            }
         </div>
     );
-}
+};
 
 export default SingleJob;
