b783557f966e0569194961730781d4df81f8b80c
adkood <ashutoshnegi195@gmail.com>
2023-12-23 19:25:55 +0530

    debouncing

--- src/App.js
+++ src/App.js
@@ -1,31 +1,27 @@
-import { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import SingleJob from './components/SingleJob';
-
 import getAllJobs from './apis/getAllJobs';
 import JobEntry from './components/JobEntry';
 import SearchBar from './components/SearchBar';
 import Signup from './components/SignUp';
-
 import { useDispatch, useSelector } from 'react-redux';
 import { modalActions } from './store';
 import useAuthState from './apis/useAuthState';
 import useLogout from './apis/useLogout';
 
 function App() {
-
   const [appRender, setAppRender] = useState(0);
+  const [searchData, setSearchData] = useState("");
   const render = useSelector((state) => state.modal.render);
   const { log } = useAuthState();
   const { logout } = useLogout();
-  // console.log(log);
   const dispatch = useDispatch();
 
   const [allJobs, setAllJobs] = useState([]);
   useEffect(() => {
     getAllJobs(setAllJobs);
   }, []);
-
 
   const ontoggle = () => {
     dispatch(modalActions.signupToggle());
@@ -34,11 +30,11 @@ function App() {
   const onLogoutHandler = () => {
     logout();
     dispatch(modalActions.changeRender());
-    setAppRender(appRender+1);
+    setAppRender(appRender + 1);
   }
 
   useEffect(() => {
-  }, [render,appRender]);
+  }, [render, appRender]);
 
   return (
     <div className="App">
@@ -47,20 +43,18 @@ function App() {
         <section className="cont__left">
           <section className='cont__left__search'>
             <h1 style={{ color: "lightseagreen", fontSize: "2rem", margin: "10px" }}>Jobs</h1>
-            <SearchBar width={"80"} height={"40"} color={'lightseagreen'} />
+            <SearchBar width={"80"} height={"40"} color={'lightseagreen'} allJobs={allJobs} setAllJobs={setAllJobs} />
             {!log && <button className='btn' onClick={ontoggle}>signup/login</button>}
             {log && <button className='btn' onClick={onLogoutHandler}>logout</button>}
           </section>
           <section className='cont__left__jobArea'>
-            {
-              allJobs && allJobs.map((ele) => {
-                return <SingleJob key={ele.id} job={ele} log={log} />
-              })
-            }
+            {allJobs && allJobs.map((ele) => (
+              <SingleJob key={ele.id} job={ele} log={log} />
+            ))}
           </section>
         </section>
         <section className="cont__right">
-          <JobEntry width={"90"} height={"85"} color={"lightseagreen"} log={log}/>
+          <JobEntry width={"90"} height={"85"} color={"lightseagreen"} log={log} />
         </section>
       </section>
     </div>
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,29 @@
-import react from 'react';
+import React, { useEffect, useState } from 'react';
 
-const SearchBar = ({width, height, color = "black"}) => {
+const SearchBar = ({ width, height, color = "black", allJobs, setAllJobs }) => {
+    const [searchData, setSearchData] = useState("");
+    // implemented Debouncing to tackle multiple search calls
+    useEffect(() => {
+        const filterJobs = () => {
+            const filteredJobs = allJobs.filter((job) =>
+                job.title.toLowerCase().includes(searchData.toLowerCase())
+            );
+            setAllJobs(filteredJobs);
+            console.log(filteredJobs);
+        };
+
+        const debounceTimeout = setTimeout(filterJobs, 1000);
+
+        return () => clearTimeout(debounceTimeout);
+    }, [searchData, setAllJobs]);
+
+    const onChangeHandler = (e) => {
+        setSearchData(e.target.value);
+    };
+
     return (
-        <div style={{borderRadius: "5px",border: `1px solid ${color}`, width: `${width}%`, height: `${height}%`, color: `${color}`}}>
-            <input placeholder='   search your role...' style={{borderRadius: "5px", width: "100%", height: "100%", color: `${color}`}} />
+        <div style={{ borderRadius: "5px", border: `1px solid ${color}`, width: `${width}%`, height: `${height}%`, color: `${color}` }}>
+            <input onChange={onChangeHandler} placeholder='   search your role...' style={{ borderRadius: "5px", width: "100%", height: "100%", color: `${color}` }} />
         </div>
     );
 }
